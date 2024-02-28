028c28aa5f5dbd09b0978d95e9d585d47f1447fa
Muntakim Rahman <muntakim.rahman@gmail.com>
2024-02-27 20:13:12 -0800

    Added Code for ADC Voltage Read. Something might work with App. Test tmrw

--- Lab4.c
+++ Lab4.c
@@ -27,9 +27,12 @@
 /* Clock Frequency and Baud Rate */
 #define SYSCLK    72000000L // SYSCLK Frequency in Hz
 #define BAUDRATE    115200L // Baudrate of UART in BPS
+#define SARCLK 18000000L // SARCLK Frequency in Hz
 
 /* Define Pins */
 #define EFM8_SIGNAL P3_3 // Signal to Measure
+#define ADC_LED P0_1 // Reference LED
+#define ADC_R P0_2 // ADC Resistor
 
 #define LCD_RS P1_7 // LCD Register Select
 #define LCD_E  P2_0 // LCD Enable
@@ -42,6 +45,8 @@
 /* Define Constants for Resistances */
 #define R_A 1.5 * KILO_MULTIPLIER
 #define R_B 1.5 * KILO_MULTIPLIER
+
+#define VDD 3.3035 // Measured VDD in Volts
 
 #define CHARS_PER_LINE 16
 
@@ -57,7 +62,7 @@ char _c51_external_startup(void) {
 	WDTCN = 0xAD; // Second Key
 
 	VDM0CN |= 0x80;
-	RSTSRC = 0x02;
+	RSTSRC=0x02|0x04;  // Enable Reset on Missing Clock Detector and VDD
 
 	#if (SYSCLK == 48000000L)
 		SFRPAGE = 0x10;
@@ -121,6 +126,83 @@ char _c51_external_startup(void) {
 	return 0;
 }
 
+void InitADC(void) {
+	SFRPAGE = 0x00;
+	ADEN=0; // Disable ADC
+
+	ADC0CN1=
+		(0x2 << 6) | // 0x0: 10-bit, 0x1: 12-bit, 0x2: 14-bit
+        (0x0 << 3) | // 0x0: No shift. 0x1: Shift right 1 bit. 0x2: Shift right 2 bits. 0x3: Shift right 3 bits.
+		(0x0 << 0) ; // Accumulate n conversions: 0x0: 1, 0x1:4, 0x2:8, 0x3:16, 0x4:32
+
+	ADC0CF0=
+	    ((SYSCLK/SARCLK) << 3) | // SAR Clock Divider. Max is 18MHz. Fsarclk = (Fadcclk) / (ADSC + 1)
+		(0x0 << 2); // 0:SYSCLK ADCCLK = SYSCLK. 1:HFOSC0 ADCCLK = HFOSC0.
+
+	ADC0CF1=
+		(0 << 7)   | // 0: Disable low power mode. 1: Enable low power mode.
+		(0x1E << 0); // Conversion Tracking Time. Tadtk = ADTK / (Fsarclk)
+
+	ADC0CN0 =
+		(0x0 << 7) | // ADEN. 0: Disable ADC0. 1: Enable ADC0.
+		(0x0 << 6) | // IPOEN. 0: Keep ADC powered on when ADEN is 1. 1: Power down when ADC is idle.
+		(0x0 << 5) | // ADINT. Set by hardware upon completion of a data conversion. Must be cleared by firmware.
+		(0x0 << 4) | // ADBUSY. Writing 1 to this bit initiates an ADC conversion when ADCM = 000. This bit should not be polled to indicate when a conversion is complete. Instead, the ADINT bit should be used when polling for conversion completion.
+		(0x0 << 3) | // ADWINT. Set by hardware when the contents of ADC0H:ADC0L fall within the window specified by ADC0GTH:ADC0GTL and ADC0LTH:ADC0LTL. Can trigger an interrupt. Must be cleared by firmware.
+		(0x0 << 2) | // ADGN (Gain Control). 0x0: PGA gain=1. 0x1: PGA gain=0.75. 0x2: PGA gain=0.5. 0x3: PGA gain=0.25.
+		(0x0 << 0) ; // TEMPE. 0: Disable the Temperature Sensor. 1: Enable the Temperature Sensor.
+
+	ADC0CF2=
+		(0x0 << 7) | // GNDSL. 0: reference is the GND pin. 1: reference is the AGND pin.
+		(0x1 << 5) | // REFSL. 0x0: VREF pin (external or on-chip). 0x1: VDD pin. 0x2: 1.8V. 0x3: internal voltage reference.
+		(0x1F << 0); // ADPWR. Power Up Delay Time. Tpwrtime = ((4 * (ADPWR + 1)) + 2) / (Fadcclk)
+
+	ADC0CN2 =
+		(0x0 << 7) | // PACEN. 0x0: The ADC accumulator is over-written.  0x1: The ADC accumulator adds to results.
+		(0x0 << 0) ; // ADCM. 0x0: ADBUSY, 0x1: TIMER0, 0x2: TIMER2, 0x3: TIMER3, 0x4: CNVSTR, 0x5: CEX5, 0x6: TIMER4, 0x7: TIMER5, 0x8: CLU0, 0x9: CLU1, 0xA: CLU2, 0xB: CLU3
+
+	ADEN=1; // Enable ADC
+}
+
+void InitPinADC(unsigned char portno, unsigned char pinno) {
+	unsigned char mask;
+
+	mask = 1 << pinno;
+
+	SFRPAGE = 0x20;
+	switch(portno) {
+		case 0:
+			P0MDIN &= (~mask); // Set Pin as Analog Input
+			P0SKIP |= mask; // Skip Crossbar Decoding for this Pin
+			break;
+		case 1:
+			P1MDIN &= (~mask); // Set Pin as Analog Input
+			P1SKIP |= mask; // Skip Crossbar Decoding for this Pin
+			break;
+		case 2:
+			P2MDIN &= (~mask); // Set Pin as Analog Input
+			P2SKIP |= mask; // Skip Crossbar Decoding for this Pin
+			break;
+		default:
+			break;
+	}
+	SFRPAGE = 0x00;
+}
+
+unsigned int ADC_at_Pin(unsigned char pin) {
+	ADC0MX = pin;   // Select the Pin for the ADC
+	ADINT = 0;
+	ADBUSY = 1;     // Convert Voltage at the Pin
+	while (!ADINT); // Wait for Conversion to Complete
+	return (ADC0);
+}
+
+float Volts_at_Pin(unsigned char pin) {
+	 return (
+		(ADC_at_Pin(pin)*VDD) / 0b_0011_1111_1111_1111
+	);
+}
+
 /*
  * Uses Timer 3 to delay <us> micro-seconds.
  */
@@ -141,14 +223,11 @@ void Timer3us(unsigned char us) {
 	TMR3CN0 = 0 ;                   // Stop Timer3 and Clear Overflow Flag
 }
 
-void waitms (unsigned int ms) {
+void waitms(unsigned int ms) {
 	unsigned int j;
-	for (j = ms; j != 0; j--) {
-		Timer3us(249);
-		Timer3us(249);
-		Timer3us(249);
-		Timer3us(250);
-	}
+	unsigned char k;
+	for (j=0; j<ms; j++)
+		for (k=0; k<4; k++) Timer3us(250);
 }
 
 void TIMER0_Init(void) {
@@ -294,6 +373,7 @@ float calculate_capacitance_nF(float period_s) {
 // }
 
 void main(void) {
+	float v_led, v_r;
 	float period_s, freq_Hz, capacitance_nF;
 	int success_count = 0;
 	// char rx_buff[CHARS_PER_LINE];
@@ -302,7 +382,20 @@ void main(void) {
     Serial_Init(); // Initialize Serial Communication
 	LCD_4BIT();	// Configure the LCD
 
+	// Initialize ADC
+	InitPinADC(0, 1); // Configure P0.1 as Analog Input
+	InitPinADC(0, 2); // Configure P0.2 as Analog Input
+	InitADC();
+
     while(1) {
+		// Measure the Voltage at the LED and Resistor
+		v_led = Volts_at_Pin(QFP32_MUX_P0_1); // Measure the Voltage at the LED
+		v_r = Volts_at_Pin(QFP32_MUX_P0_2); // Measure the Voltage at the Resistor
+
+		// Display the Voltage at the LED and Resistor
+		printf("V_LED = %f\r\n", v_led); // Print Voltage at the LED
+		printf("V_R = %f\r\n", v_r); // Print Voltage at the Resistor
+
         // Reset Counter
 		TL0 = 0;
 		TH0 = 0;
@@ -357,8 +450,6 @@ void main(void) {
 
 			waitms(250); // Wait for 250 ms
 			printf("\rC(nF) = %f\r\n", capacitance_nF); // Print Capacitance in Nanofarads
-			// printf("C(µF)=%f\r\n", (capacitance_nF / MEGA_MULTIPLIER)); // Print Capacitance in Microfarads
-
 			display_capacitance_nF(capacitance_nF);
 		}
 	}
